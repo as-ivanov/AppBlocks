55a05e13c08f3fa6a922015a6f25d2750b9b6474
as-ivanov <alexey.ivanov.lance@gmail.com>
2020-04-18 22:04:56 +0300

    update references in samples

--- src/CodeGeneration.Roslyn.MetricsCollector.Sample/IBackgroundTaskMetricsCollector.cs
+++ src/CodeGeneration.Roslyn.MetricsCollector.Sample/IBackgroundTaskMetricsCollector.cs
@@ -4,19 +4,20 @@ using MetricsCollector.Abstractions;
 
 namespace CodeGeneration.Roslyn.MetricsCollector.Sample
 {
-	[MetricsCollectorStub("BackgroundTask", "CodeGeneration.Roslyn.MetricsCollector.Sample.ISingletonDependency", "CodeGeneration.Roslyn.MetricsCollector.Sample.IMetricsCollectorImplementation")]
+	[MetricsCollectorStub("BackgroundTask", "CodeGeneration.Roslyn.MetricsCollector.Sample.ISingletonDependency",
+		"CodeGeneration.Roslyn.MetricsCollector.Sample.IMetricsCollectorImplementation")]
 	public interface IBackgroundTaskMetricsCollector
 	{
-		[MetricsCollectorMethodStub("item", "execution_count")]
-		IMeter ExecutionTotal(string taskName);
+			[MetricsCollectorMethodStub("item", "execution_count")]
+			IMeter ExecutionTotal(string taskName);
 
-		[MetricsCollectorMethodStub("item", "execution_active")]
-		ICounter ExecutionActive(string taskName);
+			[MetricsCollectorMethodStub("item", "execution_active")]
+			ICounter ExecutionActive(string taskName);
 
-		[MetricsCollectorMethodStub("ms", "execution_time")]
-		ITimer ExecutionTime(string taskName);
+			[MetricsCollectorMethodStub("ms", "execution_time")]
+			ITimer ExecutionTime(string taskName);
 
-		[MetricsCollectorMethodStub("item", "execution_error")]
-		IMeter OperationError(string key, string error);
+			[MetricsCollectorMethodStub("item1", "execution_error")]
+			IMeter OperationError(string key, string error);
 	}
 }
