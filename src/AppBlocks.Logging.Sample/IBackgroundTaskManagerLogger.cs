08d575ca1a326c212b16e2ccc562dbeac67012c1
as-ivanov <alexey.ivanov.lance@gmail.com>
2020-04-20 18:32:03 +0300

    fix: remove excess dot in log message

--- src/AppBlocks.Logging.Sample/IBackgroundTaskManagerLogger.cs
+++ src/AppBlocks.Logging.Sample/IBackgroundTaskManagerLogger.cs
@@ -7,13 +7,13 @@ namespace AppBlocks.Logging.Sample
 	[LoggerStub("AppBlocks.Logging.Sample.ISingletonDependency", "AppBlocks.Logging.Sample.ILoggerImplementation")]
 	public interface IBackgroundTaskManagerLogger
 	{
-		[LoggerMethodStub(LogLevel.Information, "Task execution started.")]
+		[LoggerMethodStub(LogLevel.Information, "Task execution started")]
 		void ExecutionStarted(string taskName);
 
-		[LoggerMethodStub(LogLevel.Information, "Task execution finished.")]
+		[LoggerMethodStub(LogLevel.Information, "Task execution finished")]
 		void ExecutionFinished(string taskName);
 
-		[LoggerMethodStub(LogLevel.Information, "Task execution failed.")]
+		[LoggerMethodStub(LogLevel.Information, "Task execution failed")]
 		void ExecutionFailed(string taskName, Exception error);
 	}
 }
