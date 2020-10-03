f681b0de19be86007bf5713afb2160bb5a1584a5
as-ivanov <alexey.ivanov.lance@gmail.com>
2020-10-03 19:18:04 +0300

    feat: clear local nuget cache when publishing new packages locally

--- publish_packages_localy.bat
+++ publish_packages_localy.bat
@@ -3,7 +3,18 @@ set local_source_path="%~dp0\LocalNuget"
 del /s /q %local_source_path%
 rmdir /s /q %local_source_path%
 
-for %%f in (%~dp0\Packages\Release\*.nupkg) do (
+for %%f in (%~dp0\Packages\Debug\*.nupkg) do (
 	nuget add %%f -source %local_source_path%
 )
+
+rd /s /q "%userprofile%\.nuget\packages\appblocks.codegeneration.attributes.common"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.logging.codegeneration.attributes"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.logging.codegeneration.roslyn"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.logging.sdk"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.monitoring.abstractions"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.monitoring.appmetrics"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.monitoring.codegeneration.attributes"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.monitoring.codegeneration.roslyn"
+rd /s /q "%userprofile%\.nuget\packages\appblocks.monitoring.sdk"
+
 
