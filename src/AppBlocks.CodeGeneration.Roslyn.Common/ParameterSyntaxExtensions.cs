6ae24d78e412a18c088f9fc812eee2fbca23e7a0
@Ivanov <alexey.Ivanov@zfx.com>
2021-04-03 15:14:52 +0300

    done

--- src/AppBlocks.CodeGeneration.Roslyn.Common/ParameterSyntaxExtensions.cs
+++ src/AppBlocks.CodeGeneration.Roslyn.Common/ParameterSyntaxExtensions.cs
@@ -11,9 +11,11 @@ namespace AppBlocks.CodeGeneration.Roslyn.Common
 		public static ParameterSyntax OmitNullableAttribute(this ParameterSyntax parameterSyntax)
 		{
 			var resultedAttributeLists = new List<AttributeListSyntax>();
+			var makeNullable = false;
 			foreach (var attributeList in parameterSyntax.AttributeLists)
 			{
 				var attributes = attributeList.Attributes.Where(_ => _.Name.ToString().Replace("global::", string.Empty) != CSharpConst.NullableAttributeName);
+				makeNullable = makeNullable || attributes.Count() < attributeList.Attributes.Count;
 				var resultedAttributeList = AttributeList(attributes.ToSeparatedList());
 				if (resultedAttributeList.Attributes.Any())
 				{
@@ -22,7 +24,7 @@ namespace AppBlocks.CodeGeneration.Roslyn.Common
 			}
 
 			return parameterSyntax
-				.WithType(NullableType(parameterSyntax.Type))
+				.WithType(makeNullable ? NullableType(parameterSyntax.Type) : parameterSyntax.Type)
 				.WithAttributeLists(List(resultedAttributeLists));
 		}
 	}
